09ac57cc517cf591c6179700d2739050998f8c1d
nqntan <ngoctan26@gmail.com>
2018-11-03 13:22:51 +0700

    update

---
+++ src/com/nqntan/algorithm/full/TestHardwoodSpecies.java
@@ -0,0 +1,16 @@
+package com.nqntan.algorithm.full;
+
+import java.util.Scanner;
+
+//https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1167
+public class TestHardwoodSpecies {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numbersOfTC = scanner.nextInt();
+        String line = null;
+        while (scanner.hasNext()) {
+            line = scanner.nextLine();
+        }
+    }
+}
---
+++ src/com/nqntan/algorithm/full/TestPlayboyChimp.java
@@ -0,0 +1,11 @@
+package com.nqntan.algorithm.full;
+
+/**
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=316&page=show_problem&problem=1552
+ */
+public class TestPlayboyChimp {
+
+    public static void main(String[] args) {
+
+    }
+}
